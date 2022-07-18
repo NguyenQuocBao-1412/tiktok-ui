f6a22d94636fb3beb9fd4db6efb734b38d0832fa
Nguyen Quoc Bao <quocbaoa6@gmail.com>
2022-07-18 22:15:09 +0700

    Cấu hình Router/Layout cho dự án

--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Following(props) {
-    return <div></div>;
+    return <h1>Following Page</h1>;
 }
 
 export default Following;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Upload(props) {
+    return <h1>Upload Page</h1>;
+}
+
+export default Upload;

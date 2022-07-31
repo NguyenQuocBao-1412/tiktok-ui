4b111a57966cc64f0674076195080e344d9b6efd
Nguyen Quoc Bao <quocbaoa6@gmail.com>
2022-07-31 23:05:23 +0700

    Xây dựng Popper

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,23 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="" alt="Alt" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Nguyen Quoc Bao</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
